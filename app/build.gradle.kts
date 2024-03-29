8ae09a93b932dcc9ed8ad3ceaf490edc9644bc03
Marcelo Benedito <marcelosilvabenedito@hotmail.com>
2024-03-29 16:25:37 -0300

    Add Compose to a View-based app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,10 +53,17 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        // Enable Jetpack Compose for this module
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
 }
 
 dependencies {
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    // other dependencies
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.core:core-ktx:1.10.1")
@@ -72,4 +79,11 @@ dependencies {
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     implementation("com.google.android.material:material:1.9.0")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    // Compose
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.material3:material3")
+    implementation("com.google.accompanist:accompanist-themeadapter-material3:0.28.0")
+
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
